c5d85b8a5319922bd857a22721c456e7dbbbf2ca
天野彰吾 <shogoa301@gmail.com>
2021-01-10 02:17:56 +0900

    コンポーネントからcontentful取得試し

--- components/Atom/purchaseBtn.vue
+++ components/Atom/purchaseBtn.vue
@@ -3,46 +3,70 @@
   <b-button class="purchaseBtn" pill variant="outline-secondary">今すぐネットで購入する</b-button>
   <!-- </div> -->
 </template>
-<script lang="ts">
+<script>
 import Vue from "vue";
 import { contentfulClient } from "~/plugins/contentful";
 
-interface Data {
-  spikeId: number;
-  spike: any;
-}
-
-export default Vue.extend({
-  data(): Data {
+export default {
+  data() {
     return {
-      spikeId: 0,
       spike: {},
+      loading: false,
     };
   },
-  async asyncData({ payload, params }) {
-    const spike = !!payload
-      ? payload
-      : await contentfulClient
-          .getEntries({
-            content_type: "spike",
-          })
-          .then((e: any) => {
-            e.items?.forEach((item: any, index: number) => {
-              console.log(index + "つめdayo");
-              console.log(item);
-            });
-
-            return e.items[0];
-          });
-
-    return { spikeId: spike.sys.id, spike };
-  },
-  created() {},
   methods: {
+    async fetch() {
+      await contentfulClient
+        .getEntries({
+          content_type: "spike",
+        })
+        .then(({ item }) => (this.spike = item))
+        .catch(console.error);
+      this.loading = false;
+    },
     externalLink() {
       const url = this.spike.fields.amazonURL;
       window.location.href = url;
     },
   },
-});
+};
+
+// interface Data {
+//   spikeId: number;
+//   spike: any;
+// }
+
+// export default Vue.extend({
+//   data(): Data {
+//     return {
+//       spikeId: 0,
+//       spike: {},
+//     };
+//   },
+//   async asyncData({ payload, params }) {
+//     const spike = !!payload
+//       ? payload
+//       : await contentfulClient
+//           .getEntries({
+//             content_type: "spike",
+//           })
+//           .then((e: any) => {
+//             e.items?.forEach((item: any, index: number) => {
+//               console.log(index + "つめdayo");
+//               console.log(item);
+//             });
+
+//             return e.items[0];
+//           });
+
+//     return { spikeId: spike.sys.id, spike };
+//   },
+//   created() {},
+//   methods: {
+//     externalLink() {
+//       const url = this.spike.fields.amazonURL;
+//       window.location.href = url;
+//     },
+//   },
+// });
 </script>
