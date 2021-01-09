9c2118224bc897ce5531a45820fe4054f8bc5b4f
井口貴文 <taka.igu.gai@gmail.com>
2021-01-10 01:35:22 +0900

    componentからもcontentfulデータ取得できそうなテスト

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -4,3 +4,10 @@ export const contentfulClient = contentful.createClient({
   space: 'ah6alnkqn7mz',
   accessToken: 'gt1GlG3SEfp8QB04J-re-pfjzGhnOk8-NYVqZANCtRQ'
 });
+
+// export `createClient` to use it in page components
+module.exports = {
+  createClient() {
+    return contentful.createClient(contentfulClient)
+  }
+}
