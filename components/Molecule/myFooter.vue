ecb22980a0eb48df9a1db5f9582141803cc701ec
天野彰吾 <shogoa301@gmail.com>
2021-01-17 02:54:02 +0900

    フッター作成

--- components/Molecule/myFooter.vue
+++ components/Molecule/myFooter.vue
@@ -1,14 +1,48 @@
 <template>
-  <div class="">
+  <div class="bgFooter">
     <ul>
-      <li>TOPへ</li>
-      <li>スパイク図鑑</li>
-      <li>こだわり比較</li>
-      <li>SMART DASH</li>
+      <nuxt-link to="/">
+        <li>TOPへ</li>
+      </nuxt-link>
+      <nuxt-link to="/spikeSearch">
+        <li>スパイク図鑑</li>
+      </nuxt-link>
+      <nuxt-link to="/spikeCompare">
+        <li>こだわり比較</li>
+      </nuxt-link>
+      <a href="https://note.com/smartdash">
+        <li>SMART DASH</li>
+      </a>
+      <div class="sepLine"></div>
+      <li>SNS</li>
+      <div class="socialBox">
+        <a href="https://www.youtube.com/?gl=JP">
+          <img
+            class="socialicon"
+            src="@/assets//img/socialicon/IG_Glyph_Fill.png"
+            alt="Instgramロゴ"
+          />
+        </a>
+        <a href="https://www.youtube.com/?gl=JP">
+          <img
+            class="socialicon"
+            src="@/assets//img/socialicon/f_logo_RGB-Blue_58.png"
+            alt="facebookロゴ"
+          />
+        </a>
+        <a href="https://www.youtube.com/?gl=JP">
+          <img
+            class="socialicon"
+            src="@/assets//img/socialicon/Twitter_Social_Icon_Circle_Color.png"
+            alt="Twitterロゴ"
+          />
+        </a>
+      </div>
     </ul>
-    <div class="sepLine"></div>
-    <p>SNS</p>
-
+    <p>©︎2021 Einja, Inc. All Rights Received</p>
+    <p>利用規約</p>
+    <p>プライバシーポリシー</p>
+    <p>運営会社</p>
   </div>
 </template>
 <script>
