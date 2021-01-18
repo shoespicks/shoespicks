cf423364b9b66acb0de129da5a554a3191e8f78d
天野彰吾 <shogoa301@gmail.com>
2021-01-18 14:05:14 +0900

    取得データから五角形作成

--- pages/spikeCompare.vue
+++ pages/spikeCompare.vue
@@ -1,45 +1,86 @@
 <template>
-  <barChart :parameter="spike" :parameter2="spike"></barChart>
+  <div>
+    <b-form @submit.prevent="submit">
+        <b-form-select v-model="spike1" class="mb-3">
+          <b-form-select-option :value="null">選べぇーーー！！</b-form-select-option>
+          <b-form-select-option-group label="Grouped options">
+            <b-form-select-option :value="spike.fields.id" v-for="spike in spikes" :key="spike.fields.id">{{ spike.fields.spikeTitle }}</b-form-select-option>
+          </b-form-select-option-group>
+        </b-form-select>
+
+        <div class="mt-2">ID: <strong>{{ spike1 }}</strong></div>
+      <b-btn type="submit">一覧取得</b-btn>
+    </b-form>
+    <b-form @submit.prevent="submit2">
+      <b-btn type="submit2">検索</b-btn>
+      <b-card
+        　v-for="spike in spikes2"
+        :title="spike.fields.spikeTitle"
+        img-alt="Image"
+        style="max-width: 50%"
+        tag="article"
+      >
+        <barChart :parameter="spike" :parameter2="spike"></barChart>
+      </b-card>
+    </b-form>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
 import { contentfulClient } from "~/plugins/contentful";
+import Vue from "vue";
 import BarChart from "@/components/Molecule/BarChart.vue";
 
 interface Data {
   spikeId: number;
-  spike: any;
+  spikes: any;
+  spikes2: any;
+  spike1: any;
 }
 
 export default Vue.extend({
   components: {
     BarChart,
   },
+
   data(): Data {
     return {
       spikeId: 0,
-      spike: {},
+      spikes: [],
+      spikes2: [],
+      spike1: null,
     };
   },
-  async asyncData({ payload, params }) {
-    const spike = !!payload
-      ? payload
-      : await contentfulClient
-          .getEntries({
-            content_type: "spike",
-            "fields.id": "chronoinx9",
-          })
-          .then((e: any) => {
-            e.items?.forEach((item: any, index: number) => {
-              console.log(item);
-            });
+  methods: {
+    submit(e: Event) {
+      e.preventDefault();
 
-            return e.items[0];
-          });
+      var searchInput: { [key: string]: string } = {
+        content_type: "spike",
+        // "fields.id[match]": this.form.name,
+      };
+      contentfulClient.getEntries(searchInput).then((e: any) => {
+        e.items?.forEach((item: any, index: number) => {
+          console.log(item);
+        });
+        this.spikes = e.items;
+      });
+    },
+    submit2(e: Event) {
+      e.preventDefault();
 
-    return { spikeId: spike.sys.id, spike };
+      var searchInput: { [key: string]: string } = {
+        content_type: "spike",
+        // "fields.id[match]": this.form.name,
+        "fields.id[match]": this.spike1,
+      };
+      contentfulClient.getEntries(searchInput).then((e: any) => {
+        e.items?.forEach((item: any, index: number) => {
+          console.log(item);
+        });
+        this.spikes2 = e.items;
+      });
+    },
   },
-  created() {},
 });
 </script>
