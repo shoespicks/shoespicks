89ba6adaefd81a1d71ae8caf509456b4fb32539d
天野彰吾 <shogoa301@gmail.com>
2021-01-24 19:37:43 +0900

    firebase導入中

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,18 +1,19 @@
-// import firebase from 'firebase/app';
-// import 'firebase/database';
-// import 'firebase/auth';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 // import 'firebase/analytics';
 
-// if (!firebase.apps.length) {
-//   firebase.initializeApp({
-//     apiKey: 'AIzaSyAKBCoMAXTPhipdNBxXOoxIc7RlBj_z-q4',
-//     authDomain: 'shoespicks-9b614.firebaseapp.com',
-//     databaseURL: 'https://shoespicks-9b614.firebaseio.com',
-//     projectId: 'shoespicks-9b614',
-//     storageBucket: 'shoespicks-9b614.appspot.com',
-//     messagingSenderId: '767094896500',
-//     appId: '1:767094896500:web:bcdf190333b172b2cede21',
-//     measurementId: 'G-95T257EXNB'
-//   });
-//   // firebase.analytics(); TODO analytics使い方わからん
-// }
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAKBCoMAXTPhipdNBxXOoxIc7RlBj_z-q4',
+    authDomain: 'shoespicks-9b614.firebaseapp.com',
+    databaseURL: 'https://shoespicks-9b614.firebaseio.com',
+    projectId: 'shoespicks-9b614',
+    storageBucket: 'shoespicks-9b614.appspot.com',
+    messagingSenderId: '767094896500',
+    appId: '1:767094896500:web:bcdf190333b172b2cede21',
+    measurementId: 'G-95T257EXNB'
+  });
+  // firebase.analytics(); TODO analytics使い方わからん
+}
+export default firebase;
