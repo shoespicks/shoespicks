4fbeacc5e2e44189061b722590fbd4faf01af684
井口貴文 <taka.igu.gai@gmail.com>
2021-02-15 21:27:56 +0900

    topこのサイトの使い方追加

--- pages/index.vue
+++ pages/index.vue
@@ -35,6 +35,40 @@
     <section class="howToUse">
       <h2>このサイトの使い方</h2>
       <!-- ただのHTML -->
+      <div class="stepCon">
+        <h3>①陸上スパイクをカンタン検索</h3>
+        <div class="stepbox">
+          <img src="@/assets//img/test-asobi/bakeru1.png" alt="画像が入るよ">
+          <p>種目、メーカー、表示順を選んでカンタンに陸上スパイクを検索。<br><br>
+             検索できるこだわり<br>
+             価格/重さ/足幅/反発性/グリップ性/ソールの傾斜</p>
+        </div>
+      </div>
+      <div class="stepCon">
+        <h3>②あなたにおすすめなスパイクをランキング表示</h3>
+        <div class="stepbox">
+          <img src="@/assets//img/test-asobi/bakeru2.png" alt="画像が入るよ">
+          <p>カンタン検索でこだわりランキング形式に表示し、あなたにおすすめな陸上スパイクを用意に発見。初心者向けスパイクや土用スパイクも絞り込み可能。</p>
+        </div>
+      </div>
+      <div class="stepCon">
+        <h3>③有力選手の評価レビューや口コミが満載</h3>
+        <div class="stepbox">
+          <img src="@/assets//img/test-asobi/bakeru3.png" alt="画像が入るよ">
+          <p>日本トップクラスの陸上選手に聞いたスパイクのレビューや、各スパイクへの口コミも充実。<br>
+             無料会員登録することで閲覧可能になります。</p>
+        </div>
+      </div>
+      <div class="stepCon">
+        <h3>④グラフでカンタンにスパイク性能比較</h3>
+        <div class="stepbox">
+          <img src="@/assets//img/test-asobi/bakeru4.png" alt="画像が入るよ">
+          <p>気になるスパイクの性能を他のスパイクとカンタンに比較可能。<br>
+          次のスパイクがどんなスパイクなのか、まずはご自分のスパイクと比べてみましょう。</p>
+        </div>
+      </div>
+
+      <b-btn class="searchBtn">まずはスパイクを検索!</b-btn>
     </section>
   </div>
 </template>
